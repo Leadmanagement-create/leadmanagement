70556e8f017a031540e42a17b736ba3b4116045c
AdityaMathur-08 <adityamathur0309@gmail.com>
2024-08-29 01:55:53 +0530

    making db_seesion async and user-service interaction with db async

--- leadmanagement1.0/viewmodels/account/account_viewmodel.py
+++ leadmanagement1.0/viewmodels/account/account_viewmodel.py
@@ -1,10 +1,16 @@
 
+from typing import Optional
 from fastapi import Request
 from viewmodels.shared.viewmodel import ViewModelBase
 from services import user_service
+from data.user import User
 
 class AccountViewModel(ViewModelBase):
     def __init__(self, request: Request):
         super().__init__(request)
+        self.user:Optional[User] = None
 
-        self.user = user_service.get_user_by_id(self.user_id)
+
+
+    async def load(self):   
+        self.user = await user_service.get_user_by_id(self.user_id)
