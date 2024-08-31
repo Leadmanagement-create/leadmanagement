782f56e0c2e6bedf6891d0b7597667e725e49df9
AdityaMathur-08 <adityamathur0309@gmail.com>
2024-09-01 00:13:58 +0530

    Added functionality to bulk upload data from csv

--- leadmanagement1.0/views/leads.py
+++ leadmanagement1.0/views/leads.py
@@ -7,6 +7,9 @@ from viewmodels.leads.lead_generation_viewmodel import LeadsGenerationViewModel
 from viewmodels.leads.lead_tracking_viewmodel import LeadsTrackingViewModel
 from services import lead_service
 from fastapi_chameleon import template
+from fastapi import UploadFile, File,HTTPException
+import csv
+import io
 
 
 #TODO Leads Routing and Leads Nurturing ViewModels and Services
@@ -32,7 +35,7 @@ async def getting_started(request:Request):
    if vm.errors:
        return vm.to_dict()
    
-   # Create the account -
+   # Create the lead -
 
    lead = await lead_service.create_lead(vm.first_name,
                                             vm.last_name,
@@ -50,3 +53,34 @@ async def getting_started(request:Request):
    cookie_auth.set_auth(response,lead.lead_id)
 
    return response
+
+@router.post("/leads/upload_leads")
+async def upload_leads(file: UploadFile = File(...)):
+    REQUIRED_COLUMNS = {'first_name','last_name','email','phone','source','status','company_id','user_id','last_contact_date'}
+    contents = await file.read()
+    leads = []
+    with io.StringIO(contents.decode('utf-8')) as file:
+        reader = csv.DictReader(file)
+        # Validate the columns
+        if not REQUIRED_COLUMNS.issubset(reader.fieldnames):
+            raise HTTPException(status_code=400,detail='Invalid CSV file: Columns are not as expected')
+        
+        for row in reader:
+            # Validate each row and create a lead 
+            # #TODO maybe use pydantic here
+            if not row['first_name'] or not row['last_name'] or not row['email'] or not row['phone'] or not row['source'] or not row['status'] or not row['company_id'] or not row['user_id'] or not row['last_contact_date']:
+                raise HTTPException(status_code=400,detail='Invalid CSV file: Missing required data')
+            
+            lead = await lead_service.create_lead(row['first_name'],
+                                                  row['last_name'],
+                                                  row['email'],
+                                                  row['phone'],
+                                                  row['source'],
+                                                  row['status'],
+                                                  row['company_id'],
+                                                  row['user_id'],
+                                                  row['last_contact_date'])
+            leads.append(lead)
+    
+    # Process the leads data as needed
+    return {"message": "Leads uploaded successfully", "leads": leads}
