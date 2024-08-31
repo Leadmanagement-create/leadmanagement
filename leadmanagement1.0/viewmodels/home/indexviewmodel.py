1a85a83186a605ee5d24973d9651a1d217327cb7
AdityaMathur-08 <adityamathur0309@gmail.com>
2024-08-31 14:59:52 +0530

    feat: Add form for lead generation and update lead services

--- leadmanagement1.0/viewmodels/home/indexviewmodel.py
+++ leadmanagement1.0/viewmodels/home/indexviewmodel.py
@@ -9,5 +9,7 @@ class IndexViewModel(ViewModelBase):
         # this was to check data on front-end coming from back-end (dynamic pages rather than static)
         #self.releases = [{'id':'login page','summary':'all new revamped login page'}, {'id':'register page','summary':'register a new user'}, {'id':'pricing options','summary':'pricing options updated'}]
         self.releases = [
-            {'id': 'aynsc database access', 'summary': 'Database session was updated to have asyncronous sessions, user-services were updated for async access'},
+            {'id': 'Aynsc database access', 'summary': 'Database session was updated to have asyncronous sessions, user-services were updated for async access'},
+            {'id': 'Adding form for lead generation', 'summary': 'A new form was added to facilitate lead generation, allowing users to submit their information for follow-up.'},
+            {'id': 'Adding lead generation data model and lead-services', 'summary': 'Introduced a new data model for lead generation and updated lead-services to handle lead data efficiently.'}
             ]
